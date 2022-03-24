22c16926db0926ca583e452e7836100ff7a1bcf1
Kilian Kistenbroker <68315043+KilianKistenbroker@users.noreply.github.com>
2022-03-24 12:41:58 -0700

    defined some level logic

--- app/src/main/java/com/example/journey_up/levels/Level_1.java
+++ app/src/main/java/com/example/journey_up/levels/Level_1.java
@@ -1,4 +1,54 @@
 package com.example.journey_up.levels;
 
-public class Level_1 {
+import android.graphics.Camera;
+import android.graphics.Canvas;
+
+import com.example.journey_up.Interface.GameInterface;
+
+public class Level_1 implements GameInterface {
+    /*
+     InGameTimerThread -> creates new runnable for counting down.
+     SortingThread -> creates new runnable for sorting objects
+     CollisionThread -> creates new runnable for collision-detections
+     LevelTouchControls -> allows for touch controls for playing levels
+     Camera -> allows for screen movement.
+     Player -> This is the player object. player.getCurrentRegion() will
+                be helpful for searching and drawing in relevant areas.
+     FinishLine -> where the player needs to land to complete the level.
+     */
+
+    public Level_1() {
+        // define the above objects
+
+        // populate the managers arrayLists with objects for player to interact with.
+
+        // run SortingThread.runSort(); must be its own thread that way loading animation remains uninterrupted.
+    }
+
+    public void receivedTouch() {}
+
+//    public Camera getCamera() {return camera;}
+
+    //beginLevel() will be called when the start button is pressed from LevelInterface.
+
+    public void beginLevelCollision() {
+        /*
+         run CollisionThread.runCollision(); must communicate with LevelInterface to determine current state
+         of game (i.e. paused, game over, etc...).
+         */
+    }
+
+    public void beginLevelTimer() {
+        // runs in-game timer thread. Must also communicate with LevelInterface to pause timer when game is paused.
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
 }
---
+++ app/src/main/java/com/example/journey_up/managers/ObjectManager.java
@@ -0,0 +1,7 @@
+package com.example.journey_up.managers;
+
+public class ObjectManager {
+    void sort(){}
+    void reset(){}
+    void quit(){}
+}
